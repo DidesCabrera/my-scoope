f9e6879498a1ac657d9b469c4d142f8d6ec7e3fa
Felipe Dides <felipe@local.dev>
2026-07-18 00:07:08 -0700

    test(onboarding): isolate legacy authenticated view cases

--- notas/tests/test_food_views.py
+++ notas/tests/test_food_views.py
@@ -2,7 +2,7 @@ import io
 import json
 
 from django.contrib.auth import get_user_model
-from django.test import Client, TestCase
+from django.test import Client, TestCase, override_settings
 from django.urls import reverse
 from openpyxl import Workbook
 
@@ -12,6 +12,7 @@ from notas.domain.models import Food
 User = get_user_model()
 
 
+@override_settings(NUTRITION_ONBOARDING_GATE_ENABLED=False)
 class FoodViewTests(TestCase):
 
     def setUp(self):
